48b25360010a4a5f38891a1385ed46257133ad84
Nursultan Unbaev <62882914+Sapolsk1@users.noreply.github.com>
2021-04-19 00:39:13 +0300

    Delete src directory

--- src/Operation.java
+++
@@ -1,4 +0,0 @@
-public interface Operation {
-    int operate(int a, int b);
-    OperationType getOperationType();
-}
--- src/OperationType.java
+++
@@ -1,6 +0,0 @@
-public enum OperationType {
-    SUBTRACTION,
-    SUMMATION,
-    MULTIPLICATION,
-    DIVISION
-}
